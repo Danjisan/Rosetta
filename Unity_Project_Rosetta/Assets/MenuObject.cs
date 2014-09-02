bf5af1732c425bf4f12c561d166cede131cf1b55
Calin <giucleac@gmail.com>
2014-09-02 22:12:22 +0300

    Created Main Menu and first build of the game
    Created MainMenu Scene, Options Scene, the Gameobject script to cycle
    through them and the first build of the game: test01

---
+++ Unity_Project_Rosetta/Assets/MenuObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuObject : MonoBehaviour {
+
+
+	public bool Quit = false;
+	public bool Options = false;
+
+	void OnMouseEnter(){
+
+		renderer.material.color = Color.red;
+	
+	}
+
+	void OnMouseExit(){
+
+		renderer.material.color = Color.white;
+	
+	}
+
+	void OnMouseDown(){
+
+		if(Quit)
+			Application.Quit ();
+
+		else
+			if(Options)
+				Application.LoadLevel ("Options Scene");
+			
+			else
+				Application.LoadLevel ("Orbit Scene");
+	
+	}
+
+}
