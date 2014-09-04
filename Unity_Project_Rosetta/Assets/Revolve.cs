3ecfcd328c581188238959feadc1fa87b5373145
Calin <giucleac@gmail.com>
2014-09-04 18:54:10 +0300

    Made Comet revolve
    Made comet 360 degrees every 12 hours through Revolve.cs script but i
    cannot increase timescale

---
+++ Unity_Project_Rosetta/Assets/Revolve.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Revolve : MonoBehaviour {
+
+	void Start() {
+		//Time.timeScale = 40;??
+	}
+
+	void Update () {
+		transform.Rotate(Vector3.down * Time.deltaTime * 0.0083333f);
+	}
+}
