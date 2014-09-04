6895dae7c10096198b8b442f91e2d77e23b2fec3
Calin <giucleac@gmail.com>
2014-09-04 23:17:26 +0300

    Minor Tweaks to Menu Scripts
    Minor Tweaks to Menu Scripts

--- Unity_Project_Rosetta/Assets/Revolve.cs
+++ Unity_Project_Rosetta/Assets/Revolve.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class Revolve : MonoBehaviour {
 
-	void Start() {
-		//Time.timeScale = 40;??
-	}
 
 	void Update () {
 		transform.Rotate(Vector3.down * Time.deltaTime * 0.0083333f);
