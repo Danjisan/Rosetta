4ae27a5c93d2af77766065a5b048e429385d6b9e
Calin <giucleac@gmail.com>
2014-09-07 01:54:51 +0300

    Fixed colliders
    Now the camera stops when coming too close to the comet (still not good)

--- Unity_Project_Rosetta/Assets/MoveCamera.cs
+++ Unity_Project_Rosetta/Assets/MoveCamera.cs
@@ -4,41 +4,34 @@ using System.Collections;
 public class MoveCamera : MonoBehaviour {
 
 	public float Speed = 5;
-	public GameObject otherObject;
-
-	Vector3 closestSurfacePoint1;
-	Vector3 closestSurfacePoint2;
-
-	void OnCollisionEnter(){
-
-		Debug.Log ("1");
-	}
-
-	void Start () {
-		// the surface point of this collider that is closer to the position of the other collider
-		closestSurfacePoint1 = collider.ClosestPointOnBounds(otherObject.transform.position);
-		
-		// the surface point of the other collider that is closer to the position of this collider
-		closestSurfacePoint2 = otherObject.collider.ClosestPointOnBounds(transform.position);
-		
-		// the distance between the surfaces of the 2 colliders
-		float surfaceDistance = Vector3.Distance(closestSurfacePoint1, closestSurfacePoint2);
-		Debug.Log(surfaceDistance);
-	}
+	private Vector3 position;
+	private bool reset = false;
+	private bool save = true;
 
 
-	void Update () {
+	void LateUpdate () {
+		if(save)
+			position = transform.position;
 
-		if (Input.GetKey("w"))
-		    transform.Translate(Vector3.forward * Time.deltaTime * Speed, Space.Self);
+		if (reset){
+			transform.position = position;
+			for(int i=0; i<3 ;i++)
+				reset = false;
+			save = true;
+		}
+
+		else{
+			if (Input.GetKey("w"))
+		   	 transform.Translate(Vector3.forward * Time.deltaTime * Speed, Space.Self);
 		    
-		if(Input.GetKey("s"))
-			transform.Translate(Vector3.back * Time.deltaTime * Speed, Space.Self);
-
-
-
-
+			if(Input.GetKey("s"))
+				transform.Translate(Vector3.back * Time.deltaTime * Speed, Space.Self);
+		}
 	}
 
+	void OnCollisionStay(){
+		reset = true;
+		save=false;
+	}
 }
 
